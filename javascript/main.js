7aa90981fc4cabab9034bf2692be17d2468a4060
Steph A <stampaa@outlook.com>
2018-06-11 17:02:43 -0500

    Added active menu when scrolling

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".row .col-sm-4 img").addClass("animated bounceIn");
 });
 
-$(function() {
-    $('li a').click(function(e) {
+$(function () {
+    $('li a').click(function (e) {
         var $this = $(this);
         $this.closest('ul').find('.active').removeClass('active');
         $this.parent().addClass('active');
     });
 });
+
+$(window).scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+    // Assign active class to nav links while scolling
+    $('.container.text-justify').each(function (i) {
+        if ($(this).position().top <= scrollDistance + 100) {
+            console.log("scrollDistance");
+            $('li').removeClass('active');
+            $('li').eq(i).addClass('active');
+        }
+    });
+}).scroll();
